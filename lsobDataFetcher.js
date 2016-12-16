252a0d77dd6975a40ab38c5e1d0c18109d45bdd6
Riccardo Coppola <Riccardo.Coppola@thetrainline.com>
2016-12-16 18:29:21 +0000

    Fetches booking numbers

--- lsobDataFetcher.js
+++ lsobDataFetcher.js
@@ -32,9 +32,13 @@ export const getAvailability = async (eventId = 68, numDays = 20) => {
   const times = availabilityMap
     .map(a => a.availability.map(time => `http://londonschoolofbarbering.simplybook.me/sheduler/load-units/?event_id=${eventId}&date=${a.date.format('YYYY-MM-DD')}&time=${time}&count=1`))
 
-  const idCalls = await Promise.all(times.map(async (urls) => await Promise.all(urls.map(url => fetch(url)))));
+  const idCalls = await Promise.all(times.map(async (urls) => {
+    const resps = await Promise.all(urls.map(url => fetch(url)));
 
-  console.log('times', idCalls);
+    return await Promise.all(resps.map(r => r.json()));
+  }));
+
+  console.log('times', JSON.stringify(_.chain(availabilityMap).zip(idCalls).value()));
 
   return availabilityMap;
 }
