b9030da8ed0de64c338ddb708c1318cc670a9aba
Riccardo Coppola <Riccardo.Coppola@thetrainline.com>
2016-12-25 18:33:37 +0100

    Refactors store as class

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,15 @@
-import { observable, computed } from 'mobx';
+import { extendObservable, computed } from 'mobx';
 
-export default observable({
-  availability: [],
-  location: 4,
-  eventId: 68,
-  fetching: false,  
-  hasAvailability: computed(function () { return this.availability.length && !this.fetching } )
-});
+export class Store {
+  constructor() {
+    extendObservable(this, {
+      availability: [],
+      location: 4,
+      eventId: 68,
+      fetching: false,
+      hasAvailability: computed(() => this.availability.length && !this.fetching)
+    });
+  }
+}
+
+export default new Store();
