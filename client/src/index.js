0fb2058d8cee60a4977b236e39ce92fde695f5dc
Riccardo Coppola <ricca509@gmail.com>
2017-01-07 11:37:33 +0000

    Uses strict mode for mobx

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { useStrict } from 'mobx';
 import { Provider } from 'mobx-react';
 import Page from './containers/AvailabilityPage/Page';
 import store from './stores/availabilityStore';
 import { startRouter } from './router';
 
+useStrict(true);
 startRouter(store);
 
 render(
